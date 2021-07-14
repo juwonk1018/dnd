4bca13785493d7c0b9256899581305058b9da218
juwonk1018 <kjon1018@gmail.com>
2021-07-14 14:59:52 +0900

    '1.0'

--- src/App.js
+++ src/App.js
@@ -14,77 +14,61 @@ import {  Button  } from 'antd';
 import 'antd/dist/antd.css';
 import { makeStyles } from '@material-ui/core/styles';
 
-function LoginButton() {
-  return (
-    <button>
-        로그인
-    </button>
-  );
-}
-function LogoutButton() {
-  return (
-      <button>
-          로그아웃
-      </button>
-);
-}
-function UserButton(props) {
-  if(props.isLoggedIn) {
-      return <LogoutButton/>;
-  }
-  return <LoginButton/>;
-}
-
 function App() {
-  const [name, setName] = useState([]);
-  const [name2, setName2] = useState([]);
+  // const [name, setName] = useState([]);
+  // const [name2, setName2] = useState([]);
+  const [testArray, setTestArray] = useState([]);
+  const [testName, setTestName] = useState([]);
+  const [testNum, setTestNum] = useState([]);
   const [show, setShow] = useState(false);
   const [ShowButton, setShowButton] = useState(true);
-  const [num1, setNum1] = useState();
-  const [num2, setNum2] = useState();
+  // const [num1, setNum1] = useState();
+  // const [num2, setNum2] = useState();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const handleShowButton = () => setShowButton(true);
   const handleClose2 = () => setShowButton(false);
-  
+  var currentCustomer = [];
+  var currentNum = [];
   var resData;
+  
   const callApi = async() =>{
     const response = await axios.get("http://localhost:3002/api");
     resData = response.data;
     resData = JSON.parse(resData);
-    var stat = [];
-    var stat2 = [];
-    setName([]);
-    setName2([]);
-
-    var numb1 = 0;
-    var numb2 = 0;
-    resData.map((item,index) => {
-      if(item.bus === "yellow"){
-        stat = stat.concat(item);
-        numb1 = numb1 + 1;
-      }
-      else{
-        stat2 = stat2.concat(item);
-        numb2 = numb2 + 1;
-      }
-      
-    });
-   
-    setName(stat);
-    setName2(stat2);
-    setNum1(stat.length);
-    setNum2(stat2.length);
+    
+    var setTest = [];
+    var setArray = [];
+    for (const [key] of Object.entries(resData)){ // Object 항목의 내용은 testArray, 이름은 testName에 push
+      setTest.push(key);
+      setArray.push(resData[key]);
+      //setNumber.push(resData.[key].length);
+    }
+    setArray.map((item,index) =>(
+      setArray[index].map((item, idx) => (
+        item.status === "승차" ?
+        currentCustomer = currentCustomer.concat(item.text):
+        (currentCustomer.indexOf(item.text)>=0? currentCustomer = currentCustomer.filter((thi)=> thi !== item.text):"")
+      )),
+      currentNum.push(currentCustomer.length)
+    ));
+    setTestNum(currentNum);
+    setTestName(setTest);
+    setTestArray(setArray);
     
   }
 
   const addData = (e) => {
     setShowButton(true);
     setShow(false);
-
     e.preventDefault();
 
-    axios.post('./api', name.concat(name2))
+    var resultObj = {};
+    testArray.map((item,index) => (
+      resultObj[testName[index]] = testArray[index]
+    ));
+
+    axios.post('./api', resultObj)
     //성공시 then 실행
     .then(function (response) {
       console.log(response);
@@ -93,33 +77,21 @@ function App() {
     .catch(function (error) {
       console.log(error);
     });
-
     window.location.reload();
-
-    
-
   }
+  // const onRemove = (index, idx) => {
+  //   let text = testArray;
+  //   var newArr = text[index].filter((thing) => thing.id !== text[index][idx].id);
+  //   text[index] = newArr;
+  //   setTestArray(text);
 
-  const onRemove1 = (id) => {
-    var stat = name.filter(name => name.id !== id);
-     setName(stat);
-     setNum1(stat.length);
-
-  }
-  const onRemove2 = (id) => {
-    var stat = name2.filter(name => name.id !== id);
-     setName2(stat);
-     setNum2(stat.length);
-
-  }
-
+  //   console.log(testArray);
+  // }
 
   const hello = () =>{
     console.log("hello")
 
   }
-
-
   const text = {
     purple: {
       color: "purple"
@@ -145,7 +117,6 @@ function App() {
 
   useEffect(() => {
     callApi();
-    
   },[ShowButton]);
 
   const useStyles = makeStyles({
@@ -167,10 +138,11 @@ function App() {
         margin: '0px 0px 0px 0px',
       },
       '.MuiListItem-root':{
+        transition : '0.25s linear',
         padding : '8px 16px 8px 8px',
         paddingLeft : ShowButton ? 5 : 8,
         marginLeft : ShowButton ? 25 : 45,
-        backgroundColor: ShowButton ? "#ffffff":"#f0f4f7",
+        backgroundColor: ShowButton ? "#ffffff":"#fcfcfc",
       },
       '.upperText':{
         width : 40,
@@ -183,17 +155,17 @@ function App() {
         border: "1px solid gray",
         backgroundColor: ShowButton ? "#ffffff" :"#f7f7f7",
       },
+      '.title':{
+        marginTop : 0,
+      }
 
       
   
       
     },
   });
-
   useStyles();
-  
-  return (
-    
+  return (    
     <div className="main">
       <span className="editButtons">
       <Button onClick={hello} variant="outline-secondary" style = {{"marginLeft" : 10}}>메모</Button>
@@ -216,154 +188,109 @@ function App() {
       </div>
  
       <div className="mainContext">
-      <DragDropContext onDragEnd={(param) => {
-        
-        const src = param.source;
-        const dest = param.destination;
-        
-        if (!dest) {
-          return;
-      }
-        if(src.droppableId === dest.droppableId){          
-          if(dest.droppableId === "droppable"){
-            name.splice(dest.index,0,name.splice(src.index,1)[0]);
+        <DragDropContext onDragEnd={(param) => {
             
-          }
-          else{
-            name2.splice(dest.index,0,name2.splice(src.index,1)[0]);
-          }
-        }
-        else{
-          var removed;
+            const src = param.source;
+            const dest = param.destination;
+            
+            if (!dest) {
+              return;
+            }
+            const idx = testName.indexOf(src.droppableId);
+            const destIdx = testName.indexOf(dest.droppableId);
+            
+            if(src.droppableId === dest.droppableId){
+              testArray[idx].splice(dest.index,0,testArray[idx].splice(src.index,1)[0]);
+            }
+
+            else{
+              var removed;
+              removed = testArray[idx].splice(src.index,1);
+              testArray[destIdx].splice(dest.index,0,removed[0]);
+            };
+            
+            currentNum = [];
+
+            testArray.map((item,index) =>(
+            testArray[index].map((item, idx) => (
+              item.status === "승차" ?
+              currentCustomer = currentCustomer.concat(item.text):
+              (currentCustomer.indexOf(item.text)>=0? currentCustomer = currentCustomer.filter((thi)=> thi !== item.text):"")
+              )),
+              currentNum.push(currentCustomer.length)
+            ))      
+            setTestNum(currentNum);
           
-          if(src.droppableId === "droppable"){
-            removed = name.splice(src.index,1);
-            removed[0].bus = "red";
-            name2.splice(dest.index,0,removed[0]);
-            //console.log(name2);
-          }
-          else{
-            removed = name2.splice(src.index,1);
-            removed[0].bus = "yellow";
-            name.splice(dest.index,0,removed[0]);
-            //console.log(name);
-          }
-          setNum1(name.length);
-          setNum2(name2.length);
-        };
-      }}
-      >
-      
-      <ul style = {{"fontSize":"10px","border":"1px solid #cacaca", "margin" : "10px 0px 0px 0px", "padding":"5px 0px 5px 5px","borderRadius" : 3,"width" : "300px", "backgroundColor" : "#f1f5fa"}}>
-      {!ShowButton &&<FiAlignJustify/> }<span style = {{"fontSize": "14px", "padding": "0px 9px 0px 10px"}}>S</span> <span style ={{"width":150}}>Yellow</span>
-      </ul>
-      <div style = {{"textAlign":"right"}}>
-      
-      <span style = {{"margin":"16px 15px 0px 0px","borderRadius":3,"float":"right","paddingRight":"0px","fontSize":"10px"}}><BsPersonFill style = {{"height":"0.8em"}}/><span style = {{"color" : (num1>6) ? "red" : "black"}}>{num1}명</span>/6명</span>
-      
-      <Droppable droppableId="droppable" isDropDisabled={ShowButton}>
-        {(provided, snapshot) => (
-          <RootRef rootRef={provided.innerRef}>
-            <List style={getListStyle(snapshot.isDraggingOver)}>
-              {name.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    
-                    <ListItem
-                      ContainerComponent="li"
-                      ContainerProps={{ ref: provided.innerRef }}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {!ShowButton && <FiAlignJustify/>}
-                      <ListItemText
-                        className = "upperText"
-                        secondary={item.text}
-                      />
-                      <ListItemText
-                        className = "status"
-                        secondaryTypographyProps={item.status === "승차"? {style: text.green} : {style: text.purple}}
-                        secondary={item.status}
-                      />
-                      {!ShowButton && <span onClick={() => onRemove1(item.id)}><AiOutlineMinus
-                        style={{"width":10}}
-                      /></span>}
-                      <ListItemSecondaryAction>
-
-                      </ListItemSecondaryAction>
-                    </ListItem>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-
-            </List>
-
+            
+          }}
+          >
+          
+          {testArray.map((item,index) =>{
+            return(    
               
-          </RootRef>
-        )}
-      </Droppable>
-      
-      </div>
-      <ul style = {{"fontSize":"10px","border":"1px solid #cacaca", "margin" : "0px 0px 0px 0px", "marginTop": num1===0 ? 30:0,"padding":"5px 0px 5px 5px","borderRadius" : 3,"width" : "300px", "backgroundColor" : "#f1f5fa"}}>
-      {!ShowButton &&<FiAlignJustify/> }<span style = {{"fontSize": "14px", "padding": "0px 9px 0px 10px"}}>1</span> <span style ={{"marginLeft":0}}>Red</span>
-      </ul>
-      <span style = {{"margin":"16px 15px 0px 0px","borderRadius":3,"paddingRight":"0px","fontSize":"10px","float" : "right"}}><BsPersonFill style = {{"height":"0.8em"}}/><span style = {{"color" : (num2>6) ? "red" : "black"}}>{num2}명</span>/6명</span>
-      
-      
-      <Droppable droppableId="droppable2" isDropDisabled={ShowButton}>
-        {(provided, snapshot) => (
-          <RootRef rootRef={provided.innerRef}>
-            <List style={getListStyle(snapshot.isDraggingOver)}>
-              {name2.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index} isDragDisabled="true">
-                  {(provided, snapshot) => (
+              <div>
+              <ul style = {{"fontSize":"10px","border":"1px solid #cacaca", "margin" : "10px 0px 0px 0px", "marginTop": (index > 0 ? (testArray[index-1].length > 0 ? 10 : 30) : 10), "padding":"5px 0px 5px 5px","borderRadius" : 3,"width" : "300px", "backgroundColor" : "#f1f5fa"}}>
+              {!ShowButton &&<FiAlignJustify stlye = {{transition : "0.25s linear"}}/> }<span style = {{"fontSize": "11px", "padding": "0px 9px 0px 10px", "marginTop":3,"fontWeight":"bold"}}>{index===0?"S":(index===testArray.length-1?"E":index)}</span> <span className="title" style ={{"fontSize": "11px","width":150}}>{testName[index]}</span>
+              </ul>
+              <span style = {{"margin":"16px 15px 0px 0px","borderRadius":3,"float":"right","paddingRight":"0px","fontSize":"10px"}}><BsPersonFill style = {{"height":"0.8em"}}/><span style = {{"color" : (testNum[index]>6) ? "red" : "black"}}>{testNum[index]}명</span>/6명</span>
+              
+              <Droppable droppableId={testName[index]} isDropDisabled={ShowButton}>
+              {(provided, snapshot) => (
+                <RootRef rootRef={provided.innerRef}>
+                  <List style={getListStyle(snapshot.isDraggingOver)}>
+                    {testArray[index].map((item, idx) => (
+                      <Draggable key={item.id} draggableId={item.id} index={idx}>
+                        {(provided, snapshot) => (
+                          
+                          <ListItem
+                            ContainerComponent="li"
+                            ContainerProps={{ ref: provided.innerRef }}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}
+                          >
+                            {!ShowButton && <FiAlignJustify/>}
+                            <ListItemText
+                              className = "upperText"
+                              secondary={item.text}
+                            />
+                            <ListItemText
+                              className = "status"
+                              secondaryTypographyProps={item.status === "승차"? {style: text.green} : {style: text.purple}}
+                              secondary={item.status}
+                              
+                            />
+                            
+                            
+                            {!ShowButton && <span><AiOutlineMinus
+                              style={{"width":10}}
+                            /></span>}
+                            <ListItemSecondaryAction>
+
+                            </ListItemSecondaryAction>
+                          </ListItem>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+
+                  </List>
+
                     
-                    <ListItem
-                      ContainerComponent="li"
-                      ContainerProps={{ ref: provided.innerRef }}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      {!ShowButton && <FiAlignJustify/>}
-                      <ListItemText 
-                        
-                        className = "upperText"
-                        secondary={item.text}
-                      />
-                      <ListItemText 
-                        className = "status"
-                        secondaryTypographyProps={item.status === "승차"? {style: text.green} : {style: text.purple}}
-                        secondary={item.status}
-                      />
-                      {!ShowButton && <span onClick={() => onRemove2(item.id)}><AiOutlineMinus
-                        style={{"width":10}}
-                      /></span>}
-                      <ListItemSecondaryAction>
-
-                      </ListItemSecondaryAction>
-                    </ListItem>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </List>
-          </RootRef>
-        )}
-      </Droppable>
-    </DragDropContext>
-
-    <div style ={{"marginTop": num2===0 ? 30:0}}></div> {/*if num2 == 0, add space to end of the block */}
-    </div>  
-  </div>    
+                </RootRef>
+              )}
+            </Droppable>
+            </div>
+            
+          )})}
+          
+        </DragDropContext>                         
+        <div style ={{"marginTop": true ? 30:0}}></div> {/*if num2 == 0, add space to end of the block */}</div>  
+    </div>    
+    
   );
 }
 export default App;
